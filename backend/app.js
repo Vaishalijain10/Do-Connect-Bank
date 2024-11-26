0f0188dd9806782ce655bf87094f796757954af3
Vaishalijain10 <vaishalijain.vj.10@gmail.com>
2024-11-26 14:35:43 +0530

    deployement setup

--- backend/app.js
+++ backend/app.js
@@ -4,16 +4,18 @@ import dotenv from "dotenv";
 import dbConnection from "./config/dbConfig.js";
 import userRouter from "./routes/userRoute.js";
 import accountRouter from "./routes/accountRoute.js";
-
 import errorHandler from "./middleware/errorHandler.js";
 import cors from "cors";
 import path from "path";
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
+0
 const app = express();
 dotenv.config();
-app.use(cors({ origin: `http://localhost:3000` }));
+
+// app.use(cors({ origin: `http://localhost:3000` }));
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:3000" }));
+
 dbConnection();
 
 app.use(express.json());
